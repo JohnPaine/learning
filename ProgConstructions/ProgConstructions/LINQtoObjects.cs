a03ac6fe528c825bc53376286eab030579231ed3
Evgeny Shtefan <shtefan@inftest.ru>
2015-11-12 17:16:33 +0300

    page 450

--- ProgConstructions/ProgConstructions/LINQtoObjects.cs
+++ ProgConstructions/ProgConstructions/LINQtoObjects.cs
@@ -11,10 +11,69 @@ namespace ProgConstructions
     {
         public static void RunTests() {
             //SimpleQueries();
-
             //SimpleQueries2();
+            //MSDNExample1();
 
-            MSDNExample1();
+            GetNamesAndDescription(ProductInfo.GetStore()).PrintCollection();
+            AlphabetizeProducts(ProductInfo.GetStore()).PrintCollection();
+            GetDiff(ProductInfo.GetStore()).PrintCollection();
+            GetIntersect(ProductInfo.GetStore()).PrintCollection();
+            GetUnion(ProductInfo.GetStore()).PrintCollection();
+        }
+
+        private static Array GetDeficientItems(IEnumerable<ProductInfo> products) {
+            Console.WriteLine("\n********** Deficient items are: **********\n");
+            var deficit = from item in products where item.NumberInStock <= 100 select item.Name;
+            var enumerable = deficit as string[] ?? deficit.ToArray();
+            enumerable.PrintCollection();
+            return enumerable.ToArray();
+        }
+
+        private static Array GetExceededItems(IEnumerable<ProductInfo> products) {
+            Console.WriteLine("\n********** Exceeded items are: **********\n");
+            var exceeded = from item in products where item.NumberInStock >= 100 select item.Name;
+            var enumerable = exceeded as string[] ?? exceeded.ToArray();
+            enumerable.PrintCollection();
+            return enumerable.ToArray();
+        }
+
+        private static Array GetUnion(IEnumerable<ProductInfo> products) {
+            Console.WriteLine("\n********** Get union between deficient and surplus products: **********\n");
+            var productInfos = products as ProductInfo[] ?? products.ToArray();
+            var deficient = GetDeficientItems(productInfos);
+            var exceeded = GetExceededItems(productInfos);
+            var diff = (from item in deficient.OfType<ProductInfo>() select item).Union(from item2 in exceeded.OfType<ProductInfo>() select item2);
+            return diff.ToArray();
+        }
+
+        private static Array GetIntersect(IEnumerable<ProductInfo> products) {
+            Console.WriteLine("\n********** Get intersect between deficient and surplus products: **********\n");
+            var productInfos = products as ProductInfo[] ?? products.ToArray();
+            var deficient = GetDeficientItems(productInfos);
+            var exceeded = GetExceededItems(productInfos);
+            var diff = (from item in deficient.OfType<ProductInfo>() select item).Intersect(from item2 in exceeded.OfType<ProductInfo>() select item2);
+            return diff.ToArray();
+        }
+
+        private static Array GetDiff(IEnumerable<ProductInfo> products) {
+            Console.WriteLine("\n********** Get diff between deficient and surplus products: **********\n");
+            var productInfos = products as ProductInfo[] ?? products.ToArray();
+            var deficient = GetDeficientItems(productInfos);
+            var exceeded = GetExceededItems(productInfos);
+            var diff = (from item in deficient.OfType<ProductInfo>() select item).Except(from item2 in exceeded.OfType<ProductInfo>() select item2);
+            return diff.ToArray();
+        }
+
+        private static Array GetNamesAndDescription(IEnumerable<ProductInfo> products) {
+            Console.WriteLine("\n********** Get names and description: **********\n");
+            var nameDesc = from p in products select new {p.Name, p.Description};
+            return nameDesc.ToArray();
+        }
+
+        private static Array AlphabetizeProducts(IEnumerable<ProductInfo> products) {
+            Console.WriteLine("\n********** Alphabetize Products: **********\n");
+            var subSet = from item in products orderby item.Name select item;
+            return subSet.ToArray();
         }
 
         private static void MSDNExample1() {
@@ -83,8 +142,7 @@ namespace ProgConstructions
             return (from car in cars where car.MaxSpeed > 240 select car).ToList();
         }
 
-        private static List<Car> GetFastBMWs(IEnumerable<Car> cars)
-        {
+        private static List<Car> GetFastBMWs(IEnumerable<Car> cars) {
             return (from car in cars where car.MaxSpeed > 240 && car.CarName == "BMW" select car).ToList();
         }
 
---
+++ ProgConstructions/ProgConstructions/ProductInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgConstructions
+{
+    internal class ProductInfo
+    {
+        public static ProductInfo[] GetStore() {
+            var items = new[] {
+                new ProductInfo {Name = "Mac's coffee", Description = "Coffee with TEETH", NumberInStock = 24},
+                new ProductInfo {Name = "Milky coffee", Description = "Coffee with milk", NumberInStock = 100},
+                new ProductInfo {Name = "Pure silk tofu", Description = "Bland as possible", NumberInStock = 120},
+                new ProductInfo {Name = "Cruchy pops", Description = "Chees and papper", NumberInStock = 2},
+                new ProductInfo {Name = "RipOff water", Description = "From the tap of your wallet", NumberInStock = 100},
+                new ProductInfo {Name = "Classic Valpo Pizza", Description = "With pizza =)", NumberInStock = 73}
+            };
+            return items;
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int NumberInStock { get; set; }
+
+        public override string ToString() {
+            return $" Name = {Name}, Description = {Description}, Number in stock = {NumberInStock} ";
+        }
+    }
+}
