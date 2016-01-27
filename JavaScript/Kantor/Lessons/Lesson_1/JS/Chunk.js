4d3a312bfa5ad65f8bc6877145cffbce34d8d473
Evgeny Shtefan <shtefan@inftest.ru>
2016-01-27 19:40:28 +0300

    Unfinished challenge Chunk

---
+++ JavaScript/Kantor/Lessons/Lesson_1/JS/Chunk.js
@@ -0,0 +1,10 @@
+/**
+ * Created by shtefan on 27.01.2016.
+ */
+function chunk(arr, size) {
+    // Break it up.
+    return arr;
+}
+
+//alert(chunk(["a", "b", "c", "d"], 2));
+
---
+++ JavaScript/Kantor/Lessons/Lesson_1/JS/Tests/Test_Chunk.js
@@ -0,0 +1,37 @@
+/**
+ * Created by shtefan on 27.01.2016.
+ */
+
+describe("chunk", function () {
+
+    describe("splits an array (first argument) into groups the length of size (second argument) and returns them as a multidimensional array", function () {
+        //before(function() { alert("Начало тестов"); });
+        //after(function() { alert("Конец тестов"); });
+        //
+        //beforeEach(function() { alert("Вход в тест"); });
+        //afterEach(function() { alert("Выход из теста"); });
+
+        //function makeTest(x, y) {
+        //    var expected = Math.pow(x, y);
+        //    it("При возведении " + x + " в степень " + y + " ожидаемый результат: " + expected, function () {
+        //        assert.equal(pow(x, y), expected);
+        //    });
+        //}
+        //
+        //for (var x = 0; x <= 11; ++x) {
+        //    for (var y = 0; y <= 11; ++y) {
+        //        makeTest(x, y);
+        //    }
+        //}
+        it('chunk(["a", "b", "c", "d"], 2) should return [["a", "b"], ["c", "d"]]', function() {
+            assert(chunk(["a", "b", "c", "d"], 2) == [["a", "b"], ["c", "d"]], 'chunk(["a", "b", "c", "d"], 2) != [["a", "b"], ["c", "d"]]');
+        });
+        it('chunk([0, 1, 2, 3, 4, 5], 3) should return [[0, 1, 2], [3, 4, 5]]', function() {
+            assert(chunk([0, 1, 2, 3, 4, 5], 3) == [[0, 1, 2], [3, 4, 5]], 'chunk([0, 1, 2, 3, 4, 5], 3) != [[0, 1, 2], [3, 4, 5]]');
+        });
+        it('chunk([0, 1, 2, 3, 4, 5], 2) should return [[0, 1], [2, 3], [4, 5]]', function() {
+            assert(chunk([0, 1, 2, 3, 4, 5], 3) == [[0, 1, 2], [3, 4, 5]], 'chunk([0, 1, 2, 3, 4, 5], 3) != [[0, 1, 2], [3, 4, 5]]');
+        });
+    });
+
+});
