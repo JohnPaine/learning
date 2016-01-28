d4f9274cc4eebd78fe15681c027d318adb2c98d2
John <johnshtefan@gmail.com>
2016-01-28 08:59:41 +0300

    Check issues with chunk!

--- JavaScript/Kantor/Lessons/Lesson_1/JS/Chunk.js
+++ JavaScript/Kantor/Lessons/Lesson_1/JS/Chunk.js
@@ -2,9 +2,19 @@
  * Created by shtefan on 27.01.2016.
  */
 function chunk(arr, size) {
-    // Break it up.
-    return arr;
+    if (size >= arr.length || size <= 0)
+        return [arr];
+    result = [];
+    var i = 0;
+    while(arr.length > i * size + size) {
+        result.push([]);
+        //result[i].push(arr.slice(i * size, i * size + size));
+        for (var j = 0; j < size && arr.length > 0; ++j) {
+            result[i].push(arr.shift());
+        }
+        i++;
+    }
+    return result;
 }
 
 //alert(chunk(["a", "b", "c", "d"], 2));
-
--- JavaScript/Kantor/Lessons/Lesson_1/JS/Tests/Test_Chunk.js
+++ JavaScript/Kantor/Lessons/Lesson_1/JS/Tests/Test_Chunk.js
@@ -4,7 +4,9 @@
 
 describe("chunk", function () {
 
-    describe("splits an array (first argument) into groups the length of size (second argument) and returns them as a multidimensional array", function () {
+    describe("splits an array (first argument) into groups " +
+        "the length of size (second argument) " +
+        "and returns them as a multidimensional array", function () {
         //before(function() { alert("Начало тестов"); });
         //after(function() { alert("Конец тестов"); });
         //
@@ -24,13 +26,24 @@ describe("chunk", function () {
         //    }
         //}
         it('chunk(["a", "b", "c", "d"], 2) should return [["a", "b"], ["c", "d"]]', function() {
+            alert(chunk(["a", "b", "c", "d"], 2));
             assert(chunk(["a", "b", "c", "d"], 2) == [["a", "b"], ["c", "d"]], 'chunk(["a", "b", "c", "d"], 2) != [["a", "b"], ["c", "d"]]');
         });
         it('chunk([0, 1, 2, 3, 4, 5], 3) should return [[0, 1, 2], [3, 4, 5]]', function() {
+            alert(chunk([0, 1, 2, 3, 4, 5], 3));
             assert(chunk([0, 1, 2, 3, 4, 5], 3) == [[0, 1, 2], [3, 4, 5]], 'chunk([0, 1, 2, 3, 4, 5], 3) != [[0, 1, 2], [3, 4, 5]]');
         });
         it('chunk([0, 1, 2, 3, 4, 5], 2) should return [[0, 1], [2, 3], [4, 5]]', function() {
-            assert(chunk([0, 1, 2, 3, 4, 5], 3) == [[0, 1, 2], [3, 4, 5]], 'chunk([0, 1, 2, 3, 4, 5], 3) != [[0, 1, 2], [3, 4, 5]]');
+            assert(chunk([0, 1, 2, 3, 4, 5], 2) == [[0, 1], [2, 3], [4, 5]], 'chunk([0, 1, 2, 3, 4, 5], 2) != [[0, 1], [2, 3], [4, 5]]');
+        });
+        it('chunk([0, 1, 2, 3, 4, 5], 4) should return [[0, 1, 2, 3], [4, 5]]', function() {
+            assert(chunk([0, 1, 2, 3, 4, 5], 4) == [[0, 1, 2, 3], [4, 5]], 'chunk([0, 1, 2, 3, 4, 5], 4) != [[0, 1, 2, 3], [4, 5]]');
+        });
+        it('chunk([0, 1, 2, 3, 4, 5, 6], 3) should return [[0, 1, 2], [3, 4, 5], [6]]', function() {
+            assert(chunk([0, 1, 2, 3, 4, 5, 6], 3) == [[0, 1, 2], [3, 4, 5], [6]], 'chunk([0, 1, 2, 3, 4, 5, 6], 3) != [[0, 1, 2], [3, 4, 5], [6]]');
+        });
+        it('chunk([0, 1, 2, 3, 4, 5, 6, 7, 8], 4) should return [[0, 1, 2, 3], [4, 5, 6, 7], [8]]', function() {
+            assert(chunk([0, 1, 2, 3, 4, 5, 6, 7, 8], 4) == [[0, 1, 2, 3], [4, 5, 6, 7], [8]], 'chunk([0, 1, 2, 3, 4, 5, 6, 7, 8], 4) != [[0, 1, 2, 3], [4, 5, 6, 7], [8]]');
         });
     });
 
